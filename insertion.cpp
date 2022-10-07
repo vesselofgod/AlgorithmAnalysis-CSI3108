2b9c56c0c88dfbbd5c5b32fc4f98220bc1b82189
vesselofgod <vesselofgod@naver.com>
2022-10-07 15:27:35 +0900

    feat: change output templete to show comfortably at insertion sort.

--- insertion.cpp
+++ insertion.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 
-__int128 cmpcnt = 0;
+__int128_t cmpcnt = 0;
 
-void printint128(__int128 x) {
+void printint128(__int128_t x) {
     if (x < 0) {
         putchar('-');
         x = -x;
@@ -18,20 +18,7 @@ void printint128(__int128 x) {
     putchar(x % 10 + '0');
 }
 
-string int128tostr(__int128 x){
-    string result = "";
-    if (x < 0) {
-        result = "-"+result;
-    }
-    if (x > 9){
-        int s = x % 10;
-        result = result+to_string(s);
-        printint128(x / 10);
-    }
-    return result;
-}
-
-__int128_t atoint128_t(std::string const & in)
+__int128_t strtoint128(std::string const & in)
 {
     __int128_t res = 0;
     size_t i = 0;
@@ -53,19 +40,59 @@ __int128_t atoint128_t(std::string const & in)
         res *= 10;
         res += c - '0';
     }
-
     if (sign){
         res *= -1;
     }
     return res;
 }
 
-void insertionsort(vector<__int128> &arr, int n){
+
+string int128tostr(__int128_t x){
+    string result = "";
+    if (x < 0) {
+        result = "-"+result;
+    }
+    if (x > 9){
+        int s = x % 10;
+        result = result+to_string(s);
+        printint128(x / 10);
+    }
+    return result;
+}
+
+void printVector(vector<__int128_t>& arr){
+    for (int i = 0; i < arr.size(); i++){
+        printint128(arr[i]);
+        cout<<"\n";
+    }
+}
+
+vector<__int128_t> initVector(string filePath){
+    ifstream readFile;
+
+    vector<__int128_t> vec;
+    __int128_t num;
+
+    readFile.open(filePath);
+    if(readFile.is_open()){
+        while (!readFile.eof()) {
+            string str;
+            getline(readFile, str);
+            num = strtoint128(str);
+            vec.push_back(num);
+        }
+        readFile.close();
+    }
+    vec.pop_back(); //remove zero value due to EOF
+    return vec;
+}
+
+void insertionsort(vector<__int128_t> &arr){
   int i, j;
-  __int128 key;
+  __int128_t key;
 
   // 인텍스 0은 이미 정렬된 것으로 볼 수 있다.
-  for(i=1; i<n; i++){
+  for(i=1; i<arr.size(); i++){
     key = arr[i]; // 현재 삽입될 숫자인 i번째 정수를 key 변수로 복사
 
     // 현재 정렬된 배열은 i-1까지이므로 i-1번째부터 역순으로 조사한다.
@@ -80,46 +107,64 @@ void insertionsort(vector<__int128> &arr, int n){
   }
 }
 
+
 int main()
 {
     clock_t start,end;
-    vector<__int128> vec;
-    __int128 num;
-    
-    ifstream readFile;
 
-    readFile.open("Q3_5");
-    ofstream fout("test.out");
-    if(readFile.is_open()){
-        while (!readFile.eof()) {
-            string str;
-            getline(readFile, str);
-            num = atoint128_t(str);
-            vec.push_back(num);
-        }
-        readFile.close();
-    }
-    int n = vec.size();
+    vector<__int128_t> q1 = initVector("Q3_1");
+    vector<__int128_t> q2 = initVector("Q3_2");
+    vector<__int128_t> q3 = initVector("Q3_3");
+    vector<__int128_t> q4 = initVector("Q3_4");
+    vector<__int128_t> q5 = initVector("Q3_5");
 
 	// sorting
     start = clock();
-    insertionsort(vec, n);
+    insertionsort(q1);
     end = clock();
-	//print(vec, n);
-
-
-
-    for (int i = 1; i < n; i++){
-        //printint128(vec[i]);
-        //cout<<" ";
-    }
-    cout<<"\n";
-
-    cout<<"runtime : "<<double(end-start)<<" millisecond"<<"\n";
-    cout<<"number of comparisons : ";
+    cout<<"Q3_1's runtime : "<<double(end-start)<<" microsecond"<<"\n";
+    cout<<"Q3_1's number of comparisons : ";
     printint128(cmpcnt);
     cout<<"\n";
-    fout.close();
+    cmpcnt = 0;
+
+    start = clock();
+    insertionsort(q2);
+    end = clock();
+    cout<<"Q3_2's runtime : "<<double(end-start)<<" microsecond"<<"\n";
+    cout<<"Q3_2's number of comparisons : ";
+    printint128(cmpcnt);
+    cout<<"\n";
+    cmpcnt = 0;
+
+    start = clock();
+    insertionsort(q3);
+    end = clock();
+    cout<<"Q3_3's runtime : "<<double(end-start)<<" microsecond"<<"\n";
+    cout<<"Q3_3's number of comparisons : ";
+    printint128(cmpcnt);
+    cout<<"\n";
+    cmpcnt = 0;
+
+    start = clock();
+    insertionsort(q4);
+    end = clock();
+    cout<<"Q3_4's runtime : "<<double(end-start)<<" microsecond"<<"\n";
+    cout<<"Q3_4's number of comparisons : ";
+    printint128(cmpcnt);
+    cout<<"\n";
+    cmpcnt = 0;
+
+    start = clock();
+    insertionsort(q5);
+    end = clock();
+    cout<<"Q3_5's runtime : "<<double(end-start)<<" microsecond"<<"\n";
+    cout<<"Q3_5's number of comparisons : ";
+    printint128(cmpcnt);
+    cout<<"\n";
+
+    //print sorted vector
+    //printVector(q4);
 
 	return 0;
 }
