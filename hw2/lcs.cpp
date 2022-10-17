0b54532d727b68dc7b9ec7823d375da4edd957f8
vesselofgod <vesselofgod@naver.com>
2022-10-18 07:55:50 +0900

    fix: Fix lcs function to correct value

--- hw2/lcs.cpp
+++ hw2/lcs.cpp
@@ -7,15 +7,10 @@
 
 using namespace std;
 
-int dp1[MAX]= {};
-int dp2[MAX]= {};
+int dp[2][MAX+1];
 string lcsString[3]={"","",""};
 
 void initString(string filePath){
-    for(int i = 0 ; i<MAX;i++){
-        dp1[i]=0;
-        dp2[i]=0;
-    }
     ifstream readFile;
     readFile.open(filePath);
     int idx = 0;
@@ -29,26 +24,22 @@ void initString(string filePath){
     return;
 }
 
-void lcs(){
-    int lena = lcsString[0].length();//can replace MAX? yes.
-    int lenb = lcsString[1].length();
-    for(int y = 0; y <= lenb; y++)
-        dp1[y] = 0; // assume x == strlenA
-    for(int x = lena - 1; x >= 0; x--){
-        // x has been decreased
-        memcpy(dp2, dp1, sizeof(dp1)); // dp[x + 1] <- dp[x]
-        dp1[lenb] = 0;
-        for(int y = lenb - 1; y >= 0 ; y--)
-            dp1[y] = (lcsString[0][x]==lcsString[1][y]) ? 1 + dp2[y+1] :
-                    max(dp2[y], dp1[y+1]);
+int lcs(){
+    int cur;
+    for (int i = 0; i <= MAX; i++){
+        cur = i % 2;
+        for (int j = 0; j <= MAX; j++){
+            if (i == 0 || j == 0) dp[cur][j] = 0;
+            else if (lcsString[0][i-1] == lcsString[1][j-1]) dp[cur][j] = dp[1 - cur][j - 1] + 1;
+            else dp[cur][j] = max(dp[1 - cur][j], dp[cur][j - 1]);
+        }
     }
-    return; // assume x == 0
+    return dp[cur][MAX];
 }
 
 
 int main()
 {
     initString("./Q1");
-    lcs();
-    cout<<"longest common sequence's length : "<<*max_element(dp1, dp1+MAX)<<endl;
+    cout<<"longest common sequence's length : "<<lcs()<<endl;
 }
